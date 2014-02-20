6a2e166a61d974f26b42f157d1e6dec94512bf4a
geekofalltrades <thegeekofalltrades@gmail.com>
2014-02-19 19:35:36 -0800

    Implemented initial unit tests and included webroot directory.

--- test_uri_parser.py
+++ test_uri_parser.py
@@ -1,9 +1,51 @@
 import unittest
-from uri_parser import map_uri
+from uri_parser import map_uri, Error404
+
 
 class TestMapUri(unittest.TestCase):
     """Test the uri mapping function. It should obtain a listing of the
     server's filesystem, check whether the file specified exists, and
     return the byte-representation of that file along with its mimetype
-    code.
+    code. These tests assume the use of the webroot directory provided
+    in the assignment spec.
     """
+    def setUp(self):
+        self.directory_requested = '/images'
+        self.image_requested = '/images/sample_1.png'
+        self.text_requested = '/sample.txt'
+        self.nonexistant_requested = '/iamnothere'
+
+    def test_directory_requested(self):
+        """Map a URI containing a request for a directory."""
+        message, mimetype = map_uri(self.directory_requested)
+       # with open(name)
+
+    def test_image_file_requested(self):
+        """Map a URI containing a request for an image file."""
+        message, mimetype = map_uri(self.image_requested)
+
+        with open('webroot' + self.image_requested, 'br') as infile:
+            expected = infile.readlines()
+
+        self.assertEqual(message, expected)
+        self.assertEqual(mimetype, 'image/png')
+
+    def test_text_file_requested(self):
+        """Map a URI containing a request for a text file."""
+        message, mimetype = map_uri(self.text_requested)
+
+        with open('webroot' + self.text_requested, 'br') as infile:
+            expected = infile.readlines()
+
+        self.assertEqual(message, expected)
+        self.assertEqual(mimetype, 'text/plain')
+
+    def test_nonexistent_resource_requested(self):
+        """Map a URI containing a request for a resource that doesn't
+        exist.
+        """
+        self.assertRaises(Error404, map_uri(self.nonexistant_requested))
+
+
+if __name__ == '__main__':
+    unittest.main()
